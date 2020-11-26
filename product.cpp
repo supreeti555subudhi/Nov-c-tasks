e90649064876b9aed9cea40b358e78cced488ee1
supreeti555subudhi <71143747+supreeti555subudhi@users.noreply.github.com>
2020-11-26 19:46:07 +0530

    Created product.cpp
    It contains the product of two float numbers

---
+++ product.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+   float fno, sno, product;
+    cout << "Enter two numbers:" ;
+	cin >> fno;
+	cin >> sno;
+    product= fno * sno;  
+	cout << "Product = " << product;    
+    return 0;
+}
